0d7bbd0e98a3a79ceec45012696f2e03ea36774b
Edwing123 <egarciavalle2014@gmail.com>
2022-07-29 19:44:34 -0600

    chore: update configuration for Vite, Typescript, Prettier
    Changes:
    
    - Set up path aliases for Vite.
    - Update `tsconfig.js` aliases.
    - Remove some settings from `.prettierrc`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@pages': path.resolve(__dirname, './src/pages/'),
+            '@components': path.resolve(__dirname, './src/components/'),
+            '@config': path.resolve(__dirname, './src/config/'),
+            '@typ': path.resolve(__dirname, './src/types/')
+        }
+    }
 })
